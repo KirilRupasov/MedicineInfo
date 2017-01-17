6dd63d2bead0d37c30f5088bd8c185d5744177ca
zveket <kirilrupasov@gmail.com>
2017-01-17 21:11:42 +0000

    updates

--- medicineApp/src/pages/signup/signup.ts
+++ medicineApp/src/pages/signup/signup.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { Auth, User, UserDetails, IDetailedError } from '@ionic/cloud-angular';
+import { MainMenu } from '../mainmenu/mainmenu';
 
 import { AlertController } from 'ionic-angular';
 
@@ -19,7 +20,7 @@ export class Signup {
   username_value: any;
   password_value: any;
 
-  constructor(public alertCtrl: AlertController, public auth: Auth, public user: User) {
+  constructor(private navCtrl: NavController, public alertCtrl: AlertController, public auth: Auth, public user: User) {
 
   }
 
@@ -33,15 +34,21 @@ export class Signup {
     let details: UserDetails = { 'username' : this.username_value, 'email': this.email_value, 'password': this.password_value};
 
     this.auth.signup(details).then(() => {
-      //success
+        this.navCtrl.push(MainMenu);
     }, (err: IDetailedError<string[]>) => {
       for (let e of err.details) {
-        if (e === 'conflict_email') {
-          errors += "Email already exists!<br>";
-        } else if(e === 'required_email') {
-          errors += "Email is required!<br>";
-        } else if(e === 'required_password') {
-          errors += "Password is required!<br>";
+        if (e === 'required_password') {
+          errors = "Password is required!<br>";
+        } else if (e === 'required_email') {
+          errors = "Email is required!<br>";
+        } else if (e === 'conflict_email') {
+          errors = "Email already exists!<br>";
+        } else if (e === 'invalid_email') {
+          errors = "Email is invalid!<br>";
+        } else if (e === 'conflict_username') {
+          errors = "Username is taken!<br>";
+        } else {
+          errors = "Unknown error...<br>";
         }
       }
 
