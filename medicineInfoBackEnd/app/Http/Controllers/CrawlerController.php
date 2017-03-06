55042539e6e178bbb9d778cbbb7a699e8f54d065
zveket <kirilrupasov@gmail.com>
2017-03-06 20:55:28 +0000

    fix

--- medicineInfoBackEnd/app/Http/Controllers/CrawlerController.php
+++ medicineInfoBackEnd/app/Http/Controllers/CrawlerController.php
@@ -107,7 +107,7 @@ class CrawlerController extends Controller {
                             $medicine = Medicine::create([
                                 'title' => $data['title'],
                                 'description' => $data['description'],
-                                //'barcodes' => $data['barcodes'],
+                                'barcodes' => $data['barcodes'],
                                 'side_effects' => $data['side_effects'],
                                 'benefits' => $data['benefits'],
                                 'elderly' => $data['elderly'],
@@ -247,7 +247,7 @@ class CrawlerController extends Controller {
             $text = $pdf->getText();
             return $this->fetchElderlyStatus($text);
         } catch (\Exception $exception) {
-            return [];
+            return "";
         }
     }
 
