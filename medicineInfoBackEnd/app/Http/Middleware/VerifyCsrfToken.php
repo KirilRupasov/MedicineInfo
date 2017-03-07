53e4d1780aef14d39bd4fca1a7bfa77c3b311ab2
zveket <kirilrupasov@gmail.com>
2017-03-07 15:30:04 +0000

    fix

--- medicineInfoBackEnd/app/Http/Middleware/VerifyCsrfToken.php
+++ medicineInfoBackEnd/app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
         '/editreview',
-        '/storereview'
+        '/storereview',
+        '/storeuser'
     ];
 }
