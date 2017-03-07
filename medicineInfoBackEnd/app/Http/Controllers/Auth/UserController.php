98083a5d5f338ca53ed4062a1944932b45946f43
zveket <kirilrupasov@gmail.com>
2017-03-07 15:24:57 +0000

    fix

--- medicineInfoBackEnd/app/Http/Controllers/Auth/UserController.php
+++ medicineInfoBackEnd/app/Http/Controllers/Auth/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
 
     }
 
-    public function createUser(Request $request) {
+    public function createUser() {
         return "blah";
         $input = $request -> all();
 
